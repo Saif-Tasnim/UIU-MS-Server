ce14714228ab37ee86316a0f105fa2cfcb92b4ec
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-08-03 14:40:31 +0600

    jwt verification done

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//verify jwt token
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
+    }
+
+    // it will carry bearer token thats why it has to split 
+    const token = authorization.split(" ")[1]
+
+    jwt.verify(token, process.env.JSON_SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' });
+        }
+
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 //testing
 app.get("/", (req, res) => {
