50de2a20e70ca70f87c24c98f5b95fe1dd676581
Saif-Tasnim <saiftasnim2002@gmail.com>
2023-08-03 14:37:18 +0600

    jwt route done

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,10 +13,48 @@ app.use(express.json());
 
 
 //testing
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     res.send("UIU MS Server is running 24/7")
 })
 
-app.listen(port , ()=>{
+// database setup info
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ectfhk2.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+        // jwt token
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.JSON_SECRET_KEY, { expiresIn: "1h" })
+            res.send({ token })
+
+        })
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
     console.log(`uiu server is using current free port : ${port}`)
 })
